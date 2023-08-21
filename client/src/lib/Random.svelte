0815cf5aaf4b45e8f3cdbad4109b86f8b8e02b63
Solaiman Mansyur <solaiman.mansyur@gmail.com>
2023-08-21 22:42:11 +0800

    add client

---
+++ client/src/lib/Random.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    let random: number = -1;
+
+    function getRandom() {
+        fetch('./api/random')
+            .then(res => res.json())
+            .then(text => random = parseFloat(text.random));
+    }
+</script>
+
+<h2>Ypur random digit is {random}</h2>
+<button on:click={getRandom}>
+    Get a new random digit
+</button>
--- server.go
+++ server.go
@@ -9,10 +9,24 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.LoadHTMLGlob("./client/**/*.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.Static("/assets", "./client/dist/assets")
+	r.StaticFile("/vite.svg", "./client/dist/vite.svg")
 
 	r.GET("/health", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"ack": time.Now(),
+		})
+	})
+
+	r.GET("/api/random", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"random": time.Now().UnixNano(),
 		})
 	})
 
