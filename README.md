ddba67d186db8af0a8e7bba8a7fccf429d6dec6b
Solaiman Mansyur <solaiman.mansyur@gmail.com>
2023-08-21 13:51:48 +0800

    the beginning

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gin + Svelte
+
+A starter template to start a new fullstack web application with Gin web framework as a backend and Svelte as a frontend. Prepared as a monorepo.
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"ack": time.Now(),
+		})
+	})
+
+	r.Run(":8080")
+}
